c6c82e1319243f95c26b2ff607d8fe34f047b175
Gleb <perveev@surfstudio.ru>
2021-12-07 23:17:44 +0300

    Угол теперь вводится в градусах и переводится автоматически в радианы

--- FigureProject/figure_interface.cpp
+++ FigureProject/figure_interface.cpp
@@ -2,11 +2,12 @@
 
 point_t IFigure::rotatePoint_(const point_t& point, const point_t& centerPoint, const double& angle)
 {
+	const double& angleDegree = toDegree_(angle);
 	return point_t(
-		centerPoint.first + (point.first - centerPoint.first) * cos(angle)
-		- (point.second - centerPoint.second) * sin(angle),
-		centerPoint.second + (point.first - centerPoint.first) * sin(angle)
-		+ (point.second - centerPoint.second) * cos(angle));
+		centerPoint.first + (point.first - centerPoint.first) * cos(angleDegree)
+		- (point.second - centerPoint.second) * sin(angleDegree),
+		centerPoint.second + (point.first - centerPoint.first) * sin(angleDegree)
+		+ (point.second - centerPoint.second) * cos(angleDegree));
 }
 
 point_t IFigure::movePoint_(const point_t& point,
@@ -14,4 +15,9 @@ point_t IFigure::movePoint_(const point_t& point,
                             const double& y)
 {
 	return point_t(point.first + x, point.second + y);
+}
+
+double IFigure::toDegree_(const double& radianValue)
+{
+	return radianValue * kMathPi / 180.0;
 }
--- FigureProject/figure_interface.h
+++ FigureProject/figure_interface.h
@@ -7,6 +7,7 @@ class IFigure
 protected:
 	static point_t rotatePoint_(const point_t& point, const point_t& centerPoint, const double& angle);
 	static point_t movePoint_(const point_t& point, const double& x, const double& y);
+	static double toDegree_(const double& radianValue);
 public:
 	virtual void rotate(const double& angle) = 0;
 	virtual void move(const double& x, const double& y) = 0;
