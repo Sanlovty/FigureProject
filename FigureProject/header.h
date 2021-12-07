b8e336c9b87ab1c5a1514c003a9724daecadf75f
Gleb <perveev@surfstudio.ru>
2021-12-07 23:17:57 +0300

    немного новых зависимостей и констант

--- FigureProject/header.h
+++ FigureProject/header.h
@@ -1,7 +1,10 @@
 #ifndef HEADER_H
 #define HEADER_H
+#include <exception>
 #include <utility>
 
+using std::exception;
 using point_t = std::pair<double, double>;
+const double kMathPi = 3.14159265358979323846;
 
 #endif
